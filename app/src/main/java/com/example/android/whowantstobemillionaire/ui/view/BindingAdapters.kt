22dda2b9cde681b7971aabab365b123447bf586d
mahmoud abou-elhamd <mahmoudabohassab00@gmail.com>
2022-09-01 01:17:20 +0200

    add submit button

--- app/src/main/java/com/example/android/whowantstobemillionaire/ui/view/BindingAdapters.kt
+++ app/src/main/java/com/example/android/whowantstobemillionaire/ui/view/BindingAdapters.kt
@@ -7,10 +7,12 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.android.whowantstobemillionaire.data.model.QuizResponse
 import com.example.android.whowantstobemillionaire.data.model.Result
+import com.example.android.whowantstobemillionaire.util.helper.add
 import com.example.android.whowantstobemillionaire.util.statue.NetworkState
 import com.skydoves.progressview.ProgressView
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
@@ -24,6 +26,10 @@ fun <T> displayLoadingState(view: View, state: NetworkState<T>?) {
 
 var listOfAnswers = listOf<String>()
 var checkedAnswers = mutableListOf<Boolean>()
+var count = 1
+var quizCoins = listOf<Int>(500,1000,2000,30000,5000,7500,10000,12500,15000,25000,50000,100000,250000,500000,1000000)
+fun getQuizNum() = count++
+fun getQuizCoins() = quizCoins[getQuizNum()]
 
 @BindingAdapter(value = ["app:displaySuccessState"])
 fun displaySuccessState(view: TextView, state: NetworkState<QuizResponse>?) {
@@ -78,11 +84,12 @@ fun handelProgressBar(progressView: ProgressView, state: NetworkState<QuizRespon
                 val observable = Observable.intervalRange(1, 30, 0, 1, TimeUnit.SECONDS)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                observable.subscribe {
+                observable.subscribe ({
                     progressView.progress = it.toFloat()
                     progressView.labelText = "$it sec"
-
-                }
+                },{
+                    e -> e.message
+                })
 
             }
         }
