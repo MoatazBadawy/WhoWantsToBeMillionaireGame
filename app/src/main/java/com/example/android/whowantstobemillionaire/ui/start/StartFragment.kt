ac7bd3e4aa9da7ae55a1460c782b476adf78d9a3
Mohammed Najm <mohammed.najm.422@gmail.com>
2022-09-05 21:25:29 +0300

    implement audio in start fragment

--- app/src/main/java/com/example/android/whowantstobemillionaire/ui/start/StartFragment.kt
+++ app/src/main/java/com/example/android/whowantstobemillionaire/ui/start/StartFragment.kt
@@ -1,18 +1,43 @@
 package com.example.android.whowantstobemillionaire.ui.start
 
+import android.media.MediaPlayer
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.navigation.Navigation
 import com.example.android.whowantstobemillionaire.R
 import com.example.android.whowantstobemillionaire.databinding.FragmentStartBinding
 import com.example.android.whowantstobemillionaire.ui.base.BaseFragment
+import com.example.android.whowantstobemillionaire.utils.Audio
 
 class StartFragment : BaseFragment<FragmentStartBinding>(R.layout.fragment_start) {
+    val audio = Audio()
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreateView() {
+        audio.runAudio(MediaPlayer.create(context, R.raw.home_audio))
         binding.startBtn.setOnClickListener { v ->
             Navigation.findNavController(v).navigate(R.id.action_startFragment_to_homeFragment)
         }
 
         binding.aboutBtn.setOnClickListener { v ->
             Navigation.findNavController(v).navigate(R.id.action_startFragment_to_aboutFragment)
+        }
+        binding.sound.setOnClickListener {
+            if(audio.muteState ==100)
+            {
+                audio.muteAudio(requireContext())
+                audio.muteState =0
+                binding.sound.setImageResource(R.drawable.ic_audio_on)
+            }
+            else{
+                audio.runAudio(MediaPlayer.create(context, R.raw.home_audio))
+                audio.unmuteAudio(requireContext())
+                audio.muteState =100
+                binding.sound.setImageResource(R.drawable.ic_audio_off)
+            }
+
+
+
+
         }
     }
 }
---
+++ app/src/main/java/com/example/android/whowantstobemillionaire/utils/Audio.kt
@@ -0,0 +1,29 @@
+package com.example.android.whowantstobemillionaire.utils
+
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+class Audio {
+    var muteState = 100
+    fun runAudio(mediaPlayer : MediaPlayer){
+        mediaPlayer.start()
+    }
+    fun pauseAudio(mediaPlayer : MediaPlayer){
+        mediaPlayer.pause()
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun muteAudio(context: Context){
+        var mAudioManager = context.getSystemService(AudioManager::class.java)
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0)
+        muteState = 0
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun unmuteAudio(context: Context){
+        var mAudioManager = context.getSystemService(AudioManager::class.java)
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 100, 0)
+        muteState = 100
+    }
+}
